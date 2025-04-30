470fb43a9f9ac85d6e62dba01fe05b6b573303c5
chitra-chauhan <chitra.webosmotic@gmail.com>
2025-04-30 15:09:02 +0530

    Update baseUrl

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -5,6 +5,8 @@ const {
   getFeedPost,
   getUsersPosts,
   getImage,
+  deletePost,
+  updatePost
 } = require("../controller/posts.controller");
 const router = express.Router();
 
@@ -12,5 +14,7 @@ router.post("/create-post", upload.single("filePath"), createPost);
 router.get("/get-feed-post", getFeedPost);
 router.get("/get-user-posts", getUsersPosts);
 router.get("/get-feed-image", getImage);
+router.delete("/delete-post/:id", deletePost);
+router.put("/update-post", updatePost);
 
 module.exports = router;
